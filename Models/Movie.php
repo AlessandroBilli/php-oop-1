968de41dbb70132549a3112d6ae34a2e761d86c8
AlessandroBilli <skillmanbungo@gmail.com>
2024-01-16 09:34:30 +0100

    feat: complete

--- Models/Movie.php
+++ Models/Movie.php
@@ -1,9 +1,34 @@
-<?php class Movie
+<?php
+
+// Definizione della classe 'Movie'
+class Movie
 {
-    public $nome;
+    // Variabili d'istanza
+    public $titolo;
     public $anno;
-    public $regista;
-    public $genere;
+    public $genere = []; // Modifica per consentire più di un genere
+    public $durata;
+
+    // Costruttore
+    public function __construct($titolo, $anno, $genere, $durata)
+    {
+        $this->titolo = $titolo;
+        $this->anno = $anno;
+        $this->genere = $genere;
+        $this->durata = $durata;
+    }
+
+    // Metodo d'istanza
+    public function stampaInfo()
+    {
+        echo "Titolo: {$this->titolo}<br>";
+        echo "Anno: {$this->anno}<br>";
+        echo "Genere: " . implode(", ", $this->genere) . "<br>"; // Modifica per gestire più di un genere
+        echo "Durata: {$this->durata} minuti<br>";
+        echo "<br>";
+    }
 }
 
-$TheGodfather = new Movie();
+// Creazione di due oggetti 'Movie'
+$film1 = new Movie("Interstellar", 2014, ["Fantascienza", "Drammatico"], 169);
+$film2 = new Movie("Inception", 2010, ["Azione", "Thriller", "Fantascienza"], 148);
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-<?php require_once __DIR__ . '/Models/Movie.php'
+<?php
 
+require_once __DIR__ . '/Models/Movie.php';
+// Stampare a schermo i valori delle relative proprietà
+echo "<h2>Film 1</h2>";
+$film1->stampaInfo();
 
-
-
-
-
-
+echo "<h2>Film 2</h2>";
+$film2->stampaInfo();
